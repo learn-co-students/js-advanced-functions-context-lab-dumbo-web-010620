4bb05171b3bcb525f1a40131318b0b3f6cb63191
wenglin1 <wenglin@buffalo.edu>
2020-03-06 14:47:07 -0500

    Done.

--- index.js
+++ index.js
@@ -94,14 +94,3 @@ let calculatePayroll = function(employeeArray) {
     }, 0)
 }
 
-// let allWagesFor = function () {
-//     let eligibleDates = this.timeInEvents.map(function (e) {
-//         return e.date
-//     })
-
-//     let payable = eligibleDates.reduce(function (memo, d) {
-//         return memo + wagesEarnedOnDate.call(this, d)
-//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-//     return payable
-// }
